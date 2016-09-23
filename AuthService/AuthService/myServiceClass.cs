d1710104f775e773130bb8b4e4c4dbe5624f607e
John Nolan <john.nolan@ilim.com>
2016-09-23 17:56:53 +0100

    Friday

--- AuthService/AuthService/myServiceClass.cs
+++ AuthService/AuthService/myServiceClass.cs
@@ -12,7 +12,8 @@ using System.Configuration;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 [WebService]
@@ -83,19 +84,6 @@ public class myServiceClass : System.Web.Services.WebService
     {
         var collection = getMongoCollection("users");
 
-        /*
-        using (var cursor = await collection.Find(new BsonDocument()).ToCursorAsync())
-        {
-            while (await cursor.MoveNextAsync())
-            {
-                foreach (var doc in cursor.Current)
-                {
-                    Console.WriteLine(doc);
-                }
-            }
-        }
-        */
-
         var builder = Builders<BsonDocument>.Filter;
         var filter = builder.Eq("username", userID) & builder.Eq("pin", pin);
         var update = Builders<BsonDocument>.Update
@@ -126,17 +114,59 @@ public class myServiceClass : System.Web.Services.WebService
 
         var res2 = collection.Find(filter).ToList();
 
-        if (res2.Count() > 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
+        return (res2.Count() > 0);
     }
 
+    static string getUserPolicyList(string userID, string pin)
+    {
+        var collection = getMongoCollection("users");
+
+        var filter = Builders<BsonDocument>.Filter.Eq("username", userID) & Builders<BsonDocument>.Filter.Eq("pin", pin);
+        var projection = Builders<BsonDocument>.Projection
+            .Exclude("_id")
+            .Include("policies.policyID")
+            .Include("policies.policyType");
+
+        var x = collection.Find(filter).Project(projection).First().ToJson();
+
+        return JObject.Parse(x)["policies"].ToString();
+    }
+
+    static string getUserPolicyDetails(string userID, string pin, string policyID)
+    {
+        var collection = getMongoCollection("users");
+
+        var filter = Builders<BsonDocument>.Filter.Eq("username", userID) &
+                     Builders<BsonDocument>.Filter.Eq("pin", pin) &
+                     Builders<BsonDocument>.Filter.Eq("policies.policyID", policyID);
+        var projection = Builders<BsonDocument>.Projection
+            .Exclude("_id")
+            .Include("policies");
+
+        var x = collection.Find(filter).Project(projection).First().ToJson();
+
+        return JObject.Parse(x)["policies"].ToString();
+    }
+
+    static bool addPolicyToUser(string userID, string pin, string policyID)
+    {
+        var policyCollection = getMongoCollection("policies");
+
+        var builder = Builders<BsonDocument>.Filter;
+        var policyFilter = builder.Eq("policyID", policyID);
+        var projection = Builders<BsonDocument>.Projection
+            .Exclude("_id");
+        var policyDoc = policyCollection.Find(policyFilter).Project(projection);
+
+        if (policyDoc.First() == null) return false;
+
+        var userCollection = getMongoCollection("users");
+        var userFilter = builder.Eq("username", userID) & builder.Eq("pin", pin);
+
+        return (userCollection.UpdateOne(userFilter, Builders<BsonDocument>.Update.Push("policies", policyDoc.First())).ModifiedCount > 0);
+
+        //TODO: remove policy from policies collection once it's been added to a user.
+    }
 
     class Person
     {
@@ -153,9 +183,7 @@ public class myServiceClass : System.Web.Services.WebService
     static async Task CallMain()
     {
         const string ConnectionString = "mongodb://etsid005181.europa.internal:27017";
-        //var server = MongoServer.Create(ConnectionString);
         var Client = new MongoClient(ConnectionString);
-        // var blog = server.GetDatabase("tenxtest");
         var blog = Client.GetDatabase("tenxtest");
         var collection = blog.GetCollection<BsonDocument>("users");
 
@@ -220,41 +248,58 @@ public class myServiceClass : System.Web.Services.WebService
         HttpContext.Current.Response.End();
     }
 
-
     [WebMethod]
     [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
     public void SaveUserDetails(string userID, string pin, string email, string mobile, string address)
     {
-
         updateUser(userID, pin, email, mobile, address).Wait();
 
         var jsonData = new
         {
-            authenticated = true
+            updateSuccessful = true
         };
 
-
-        var serializer = new JavaScriptSerializer();
-        var json = serializer.Serialize(jsonData);
+        var json = new JavaScriptSerializer().Serialize(jsonData);
 
         HttpContext.Current.Response.Write(json);
         HttpContext.Current.Response.End();
     }
 
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public void GetUserPolicyDetails(string userID, string pin, string policyID)
+    {
+        HttpContext.Current.Response.Write(getUserPolicyDetails(userID, pin, policyID));
+        HttpContext.Current.Response.End();
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public void GetUserPolicyList(string userID, string pin)
+    {
+        HttpContext.Current.Response.Write(getUserPolicyList(userID, pin));
+        HttpContext.Current.Response.End();
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public void AddPolicyToUser(string userID, string pin, string policyID)
+    {
+        var jsonData = JsonConvert.SerializeObject(new
+        {
+            updateSuccessful = addPolicyToUser(userID, pin, policyID)
+        });
+
+        HttpContext.Current.Response.Write(jsonData);
+        HttpContext.Current.Response.End();
+    }
+
     public void AddHeaders(string origin)
     {
-
         HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", origin);
         HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE");
         HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
         HttpContext.Current.Response.AddHeader("Access-Control-Allow-Credentials", "true");
         HttpContext.Current.Response.ContentType = "application/json";
-
     }
-
-    public class LoggedInUserData
-    {
-        public String Message;
-    }
-
 }
